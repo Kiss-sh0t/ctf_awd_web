7b5561821362c102db25ab9fe695c7c1ddf9719d
Kiss-sh0t <u511@U511deMacBook-Air.local>
2017-12-14 09:29:50 +0800

    python web logs

---
+++ logs/plogs.py
@@ -0,0 +1,28 @@
+import time
+from logging import getLogger
+
+#settings/base.py MIDDLEWARE_CLASSES+=django.middleware.plogs.LoggingMiddleware
+class LoggingMiddleware(object):
+    def __init__(self):
+        # arguably poor taste to use django's logger
+        self.logger = getLogger('django.request')
+
+    def process_request(self, request):
+        #request.timer = time()
+        t = time.strftime("%H:%M:%S", time.localtime())
+
+        ip = request.META['REMOTE_ADDR'] or ''
+        ua = request.META['HTTP_USER_AGENT'] or ''
+        cookie = request.META['HTTP_COOKIE'] or ''
+        method = request.META['REQUEST_METHOD'] or ''
+        body = request.body or ''
+        if (request.META['QUERY_STRING']==''):
+            url = request.META['PATH_INFO']
+        else:
+            url = request.META['PATH_INFO']+'?'+request.META['QUERY_STRING'] or ''
+        log = '[%s]--[IP:%s]--Method:%s--url:%s--POST:%s--UA:%s--cookie:%s'%(t,ip, method,url,body,ua,cookie)
+        self.logger.info(log)
+        return None
+
+    def process_response(self, request, response):
+        return response
